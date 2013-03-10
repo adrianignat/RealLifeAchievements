7ff817c48b6429e775a521576f2b32632448e238
Adrian Ignat <adrian.ignat@gmail.com>
2013-03-10 17:11:33 +0200

    Added Views for Map and Achievements

--- src/com/example/reallifeachievements/MainActivity.java
+++ src/com/example/reallifeachievements/MainActivity.java
@@ -1,8 +1,10 @@
-package com.aidev.reallifeachievements;
+package com.example.reallifeachievements;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,20 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void ViewMapButtonClick(View view)
+	{
+		Intent intent = new Intent(this, ViewMapActivity.class);		
+		
+		startActivity(intent);
+	}
+	
+	public void AchievementsButtonClick(View view)
+	{
+		Intent intent = new Intent(this, ViewAchievementsActivity.class);		
+		
+		startActivity(intent);
 	}
 
 }
